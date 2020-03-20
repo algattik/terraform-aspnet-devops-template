7376420a4219c2c68514a5e1df1277972f60d144
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-03-20 20:39:42 +0100

    Update devops_agent_init.sh

--- infrastructure/terraform-shared/devops-agent/devops_agent_init.sh
+++ infrastructure/terraform-shared/devops-agent/devops_agent_init.sh
@@ -32,7 +32,8 @@ apt-get install -y --no-install-recommends \
         ca-certificates \
         jq \
         apt-transport-https \
-        docker.io
+        docker.io \
+        unzip
 
 
 echo "Allowing agent to run docker"
