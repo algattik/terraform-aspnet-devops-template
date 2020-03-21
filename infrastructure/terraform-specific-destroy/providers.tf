7b80408f434c106c753cc02c251cc666b5113849
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-03-21 10:55:11 +0100

    .

---
+++ infrastructure/terraform-specific-destroy/providers.tf
@@ -0,0 +1,17 @@
+#Set the terraform required version
+
+terraform {
+  required_version = ">= 0.12.6"
+}
+
+# Configure the Azure Provider
+
+provider "azurerm" {
+  # It is recommended to pin to a given version of the Provider
+  version = "=2.2.0"
+  skip_provider_registration = true
+  features {}
+}
+
+provider "kubernetes" {
+}
