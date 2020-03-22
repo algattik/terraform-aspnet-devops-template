4bc75dc6c4a121874115865bf8a32da5e4b26653
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-03-22 07:28:08 +0100

    .

--- infrastructure/terraform-shared/devops-agent/install_software.sh
+++ infrastructure/terraform-shared/devops-agent/install_software.sh
@@ -11,9 +11,9 @@ apt-get install openjdk-8-jdk
 
 # Install the .NET Core SDK, for test coverage report generation
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo add-apt-repository universe
-sudo apt-get update
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install dotnet-sdk-3.1
+dpkg -i packages-microsoft-prod.deb
+add-apt-repository universe
+apt-get update
+apt-get install apt-transport-https
+apt-get update
+apt-get install dotnet-sdk-3.1
