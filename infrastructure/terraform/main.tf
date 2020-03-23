a6087ee225011d5f3e4d9de5d377baaac2637d13
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-03-23 07:04:22 +0100

    .

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -13,3 +13,18 @@ resource "kubernetes_namespace" "build" {
     name = var.kubernetes_namespace
   }
 }
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus-operator"
+  chart      = "stable/prometheus-operator"
+  namespace  = var.kubernetes_namespace
+
+  wait       = true
+  timeout    = 300
+
+  # Workaround for https://github.com/helm/charts/issues/19452
+  set {
+    name  = "prometheusOperator.createCustomResource"
+    value = false
+  }
+}
