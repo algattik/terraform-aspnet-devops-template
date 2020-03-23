55396f16559936417bcd2ab9acb0113808f90579
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-03-23 06:16:27 +0100

    .

--- Src/Contoso.UnitTests/Services/SampleServiceTests.cs
+++ Src/Contoso.UnitTests/Services/SampleServiceTests.cs
@@ -5,7 +5,6 @@
 namespace Contoso.UnitTests
 {
     using Contoso;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
     using NSubstitute;
     using Xunit;
@@ -13,14 +12,14 @@ namespace Contoso.UnitTests
     public class SampleServiceTests
     {
         [Fact]
-        public void AddTwoNumbers()
+        public async void AddTwoNumbers()
         {
             var controller = Substitute.For<ISampleController>();
             controller.SumNumbersUpTo(2).Returns(3);
             var service = new SampleService(
                 controller,
                 Substitute.For<ILogger<SampleService>>());
-            var returnedString = service.SumNumbersUpTo(3);
+            var returnedString = await service.SumNumbersUpToAsync(3);
             Assert.Equal(6, returnedString);
         }
     }
