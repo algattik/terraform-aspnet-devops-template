449cb805c8e4ee41ccb99f1008c433bfd80e40bc
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-03-24 17:21:28 +0100

    Changed mocking framework to Moq

--- Src/Contoso.UnitTests/Services/SampleServiceTests.cs
+++ Src/Contoso.UnitTests/Services/SampleServiceTests.cs
@@ -4,9 +4,10 @@
 
 namespace Contoso.UnitTests
 {
+    using System.Threading.Tasks;
     using Contoso;
     using Microsoft.Extensions.Logging;
-    using NSubstitute;
+    using Moq;
     using Xunit;
 
     public class SampleServiceTests
@@ -14,12 +15,14 @@ namespace Contoso.UnitTests
         [Fact]
         public async void AddTwoNumbers()
         {
-            var controller = Substitute.For<ISumComputationAPI>();
-            controller.SumNumbersUpTo(2).Returns(3);
+            var controller = new Mock<ISumComputationAPI>();
+
+            controller.Setup(foo => foo.SumNumbersUpTo(2)).Returns(Task.FromResult(3));
+
             var service = new SampleService(
-                controller,
-                Substitute.For<ILogger<SampleService>>(),
-                Substitute.For<MetricsService>());
+                controller.Object,
+                new Mock<ILogger<SampleService>>().Object,
+                new Mock<MetricsService>(null).Object);
             var returnedString = await service.SumNumbersUpToAsync(3);
             Assert.Equal(6, returnedString);
         }
