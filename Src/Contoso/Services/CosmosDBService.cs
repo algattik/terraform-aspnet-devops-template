22054df654e9ae585611e545defacbd98f6d9e30
Alexandre Gattiker <algattik@users.noreply.github.com>
2020-03-26 14:43:23 +0100

    Update CosmosDBService.cs

--- Src/Contoso/Services/CosmosDBService.cs
+++ Src/Contoso/Services/CosmosDBService.cs
@@ -5,6 +5,7 @@
 namespace Contoso
 {
     using System;
+    using System.Linq;
     using System.Globalization;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos;
@@ -20,6 +21,10 @@ namespace Contoso
     /// </summary>
     public class CosmosDBService : ICosmosDBService
     {
+        private readonly Container container;
+
+        private readonly string topic;
+
         private readonly ILogger<CosmosDBService> logger;
 
         /// <summary>
@@ -40,11 +45,54 @@ namespace Contoso
                 throw new ArgumentNullException(nameof(azure));
             }
 
-            var containerId = configuration["cosmosDBContainer"];
+            var cosmosDBContainerResourceId = configuration["cosmosDBContainer"];
 
-            logger.LogWarning(
-                "Skipping connection to Cosmos DB container {containerId} (not yet implemented)",
-                containerId);
+            logger.LogInformation(
+                "Initializing connection to Event Hub {cosmosDBContainerResourceId} topic {topic}",
+                cosmosDBContainerResourceId,
+                this.topic);
+
+            ParseCosmosDBContainerResourceId(
+                cosmosDBContainerResourceId,
+                out string cosmosDBResourceId,
+                out string cosmosDBDatabase,
+                out string cosmosDBContainer);
+
+            ICosmosDBAccount cosmosDB;
+            try
+            {
+                cosmosDB = azure
+                    .Azure
+                    .CosmosDBAccounts
+                    .GetById(cosmosDBResourceId);
+            }
+            catch (Exception e)
+            {
+                logger.LogCritical(
+                    e,
+                    "Couldn't retrieve Cosmos DB account keys for rule {cosmosDBResourceId}",
+                    cosmosDBResourceId);
+                throw;
+            }
+
+            try
+            {
+                var client = new CosmosClientBuilder(
+                    cosmosDB.DocumentEndpoint,
+                    cosmosDB.ListKeys().PrimaryMasterKey)
+                    .Build();
+                this.container = client
+                    .GetDatabase(cosmosDBDatabase)
+                    .GetContainer(cosmosDBContainer);
+            }
+            catch (Exception e)
+            {
+                logger.LogCritical(
+                    e,
+                    "Couldn't retrieve authorization keys for rule {cosmosDBContainerResourceId}",
+                    cosmosDBContainerResourceId);
+                throw;
+            }
 
             this.logger = logger;
         }
@@ -55,11 +103,26 @@ namespace Contoso
         /// <param name="value">Value up to which the sum is computed.</param>
         /// <param name="sum">Sum of numbers from 0 to value.</param>
         /// <returns>Cosmos DB operation result.</returns>
-        public Task PersistSum(long value, long sum)
+        public async Task<ItemResponse<ComputedSum>> PersistSum(long value, long sum)
         {
-            this.logger.LogWarning(
-                "Saving to Cosmos DB not yet implemented");
-            return Task.FromResult(0);
+            var e = new ComputedSum
+            {
+                Id = value.ToString(CultureInfo.InvariantCulture),
+                Sum = sum,
+            };
+            return await this.container.ReplaceItemAsync(e, e.Id);
+        }
+
+        public static void ParseCosmosDBContainerResourceId(
+                string cosmosDBContainerResourceId,
+                        out string cosmosDBResourceId,
+                        out string cosmosDBDatabase,
+                        out string cosmosDBContainer)
+        {
+            var items = cosmosDBContainerResourceId.Split('/');
+            cosmosDBResourceId = string.Join('/', items.Take(4));
+            cosmosDBDatabase = items[5];
+            cosmosDBContainer = items[6];
         }
     }
 }
